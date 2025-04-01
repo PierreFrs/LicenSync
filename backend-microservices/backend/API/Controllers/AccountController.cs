0d9ef54a88d7cde1a686d3471cf6382ef675ce6f
Pierre <pierrefraisse90@gmail.com>
2025-04-01 16:54:45 +0200

    feat(accountcontroller): Added login after successful register

--- backend-microservices/backend/API/Controllers/AccountController.cs
+++ backend-microservices/backend/API/Controllers/AccountController.cs
@@ -36,6 +36,14 @@ public class AccountController(SignInManager<AppUser> signInManager, IMapper map
         if (result.Succeeded)
         {
             await signInManager.UserManager.AddToRoleAsync(user, "User");
+
+            var loginDto = new LoginDto
+            {
+                Email = registerDto.Email,
+                Password = registerDto.Password,
+            };
+
+            Login(loginDto);
             return Ok();
         }
 
