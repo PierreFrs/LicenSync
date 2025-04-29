0f95eea4b474458ce14a1538e14828c0a0ed8f6f
Pierre <pierrefraisse90@gmail.com>
2025-04-29 17:35:40 +0200

    feat(front tests): updated tests

--- frontend/src/app/features/pages/user-page/track/track.component.spec.ts
+++ frontend/src/app/features/pages/user-page/track/track.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TrackComponent } from './track.component';
 import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
 import {Observable, of} from 'rxjs';
