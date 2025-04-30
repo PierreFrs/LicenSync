0e22cedb855ea6677bd4958b47cf6e7c4e4d5a7c
Pierre <pierrefraisse90@gmail.com>
2025-04-30 16:57:32 +0200

    feat(upload link): updated album upload link in upload component, header and footer

--- frontend/src/app/shared/upload-link/upload-link.component.ts
+++ frontend/src/app/shared/upload-link/upload-link.component.ts
@@ -1,31 +1,12 @@
-import {Component, inject, Input} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
-import {
-  TrackUploadDialogComponent
-} from "../../features/pages/user-page/user-overview/track-upload-dialog/track-upload-dialog.component";
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-upload-link',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './upload-link.component.html'
 })
-export class UploadLinkComponent {
-  private dialog = inject(MatDialog);
-
-  @Input() userId: string | null = null;
-
-  openUploadDialog() {
-    if (this.userId) {
-      this.dialog.open(TrackUploadDialogComponent, {
-        panelClass: "modal-panel",
-        data: {userId: this.userId}
-      });
-    }
-    else {
-      console.error(
-        "UploadLinkComponent: userId is null"
-      );
-    }
-  }
-}
+export class UploadLinkComponent {}
